8d8d43407fbd9b9cf9c9832aba22175250a061b7
VeRTeXR <p5vasut27665@gmail.com>
2020-04-24 22:42:43 +0700

    - Add Input Handler - Add Move Position Data class

---
+++ SnakeRPG/Assets/Scripts/MovePosition.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MovePosition
+{
+    private MovePosition _previousMovePosition;
+    private Vector2Int gridPosition;
+    private Direction direction;
+
+    public MovePosition(MovePosition previousMovePosition, Vector2Int gridPosition, Direction direction) {
+        this._previousMovePosition = previousMovePosition;
+        this.gridPosition = gridPosition;
+        this.direction = direction;
+    }
+
+    public Vector2Int GetGridPosition() {
+        return gridPosition;
+    }
+
+    public Direction GetDirection() {
+        return direction;
+    }
+
+    public Direction GetPreviousDirection() {
+        if (_previousMovePosition == null) {
+            return Direction.Right;
+        } else {
+            return _previousMovePosition.direction;
+        }
+    }
+}
